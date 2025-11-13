3f9d7198e8d56358073d76d4e023691ef65bf6f3
modmuss50 <modmuss50@gmail.com>
2025-11-13 22:12:34 +0000

    Setup workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { comlink } from "vite-plugin-comlink";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [comlink(), react()],
+  worker: {
+    plugins: () => [comlink()],
+  },
 })
